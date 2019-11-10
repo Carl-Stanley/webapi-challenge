174a176e631754660e950a5ad995f6113e02c690
Carl-Stanley <cds.beml@gmail.com>
2019-11-10 12:08:18 -0700

    Commit

--- stretch/src/App.js
+++ stretch/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import ProjectsList from './components/ProjectsList';
 
+import { Route, Redirect } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-       
-      <ProjectsList /> 
-      
+            
+      <Route path="/projectslist" component={ProjectsList} />
+		
     </div>
   );
 }
